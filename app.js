a7d6cf70ecd10464cefbef9a113cb00fbcda4654
Khoi01~ <nhhoangkhoi@gmail.com>
2024-01-05 15:40:47 +0700

    Update endpoints

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const indexRouter = require('./routes/index');
-const mongoose = require('mongoose');
-require('dotenv/config');
+const { sendResponse, AppError } = require("./helpers/utils");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MONGODB
-mongoose.connect(process.env.MONGO_URI, () => {
-	console.log('Connected to Database!');
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log(`DB connected ${mongoURI}`))
+  .catch((err) => console.log(err));
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const err = new AppError(404, "Not Found", "Bad Request");
+  next(err);
 });
 
-app.use('/', indexRouter);
+app.use((err, req, res, next) => {
+  console.log("ERROR", err);
+  return sendResponse(
+    res,
+    err.statusCode ? err.statusCode : 500,
+    false,
+    null,
+    { message: err.message },
+    err.isOperational ? err.errorType : "Internal Server Error"
+  );
+});
 
 module.exports = app;
--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,91 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
+const { sendResponse, AppError } = require("../helpers/utils.js");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const newCar = req.body;
+    if (!newCar) throw new AppError(402, "Bad Request", "Create new car Error");
+
+    const insertNewCar = await Car.create(newCar);
+
+    sendResponse(
+      res,
+      200,
+      true,
+      { data: insertNewCar },
+      null,
+      "Create Car Successfully!"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const filter = {};
+    const carsPerPage = 10;
+    const pageNumber = req.query.page || 1;
+    const carTotal = await Car.count(filter);
+    const carTotalPages = Math.ceil(carTotal / carsPerPage);
+    const getCars = await Car.find(filter)
+      .sort({ _id: 1 })
+      .skip(pageNumber > 0 ? (pageNumber - 1) * carsPerPage : 0)
+      .limit(carsPerPage);
+
+    const listCars = { cars: getCars, total: carTotalPages };
+
+    sendResponse(res, 200, true, listCars, null, "Get Car List Successfully!");
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const targetId = req.params.id;
+    console.log("id", targetId);
+
+    const updateInfo = req.body;
+    console.log("updateinfo", updateInfo);
+    const options = { new: true };
+    const updateCar = await Car.findByIdAndUpdate(
+      targetId,
+      updateInfo,
+      options
+    );
+    sendResponse(
+      res,
+      200,
+      true,
+      { data: updateCar },
+      null,
+      "Update Car Successfully"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const targetId = req.params.id;
+    const options = { new: true };
+
+    const deleteCar = await Car.findByIdAndDelete(targetId, options);
+    sendResponse(
+      res,
+      200,
+      true,
+      { data: deleteCar },
+      null,
+      "Delete Car Successfully"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = carController;
